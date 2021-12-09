2ec7e85c0fcf8d5564a9d5f2316480bdbb1acf3f
서진교 <sjg@gabia.com>
2021-12-09 15:48:47 +0900

    commit project

---
+++ Server/src/main/java/com/dyago/DiegoShopping/DiegoShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.dyago.DiegoShopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiegoShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiegoShoppingApplication.class, args);
+	}
+
+}
---
+++ Server/src/test/java/com/dyago/DiegoShopping/DiegoShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dyago.DiegoShopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiegoShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
