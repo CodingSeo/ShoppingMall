9d2e18b1cf7701fe0cace4ad4ed52afeadd7b1f5
서진교 <sjg@gabia.com>
2021-12-09 16:38:40 +0900

    - LoginUser, User baseEntity wjrdyd

--- Server/src/main/java/com/dyago/DiegoShopping/domains/users/entities/LoginUser.java
+++ Server/src/main/java/com/dyago/DiegoShopping/domains/users/entities/LoginUser.java
@@ -3,15 +3,9 @@ package com.dyago.DiegoShopping.domains.users.entities;
 import com.dyago.DiegoShopping.commons.BaseEntity;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity(name="users")
 public class LoginUser extends BaseEntity {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
     private String email;
     private String password;
 }
--- Server/src/main/java/com/dyago/DiegoShopping/domains/users/entities/User.java
+++ Server/src/main/java/com/dyago/DiegoShopping/domains/users/entities/User.java
@@ -1,20 +1,15 @@
 package com.dyago.DiegoShopping.domains.users.entities;
 
+import com.dyago.DiegoShopping.commons.BaseEntity;
 import com.dyago.DiegoShopping.domains.users.enums.Gender;
 import lombok.Getter;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+public class User extends BaseEntity {
     private String name;
     private String email;
     private String phone;
